8ef143c0d8a0572a84985ce180b93d57ba77ccfb
William <wjones@clearc2.com>
2023-10-25 11:46:34 -0500

    add intersection example

--- src/01-unions/01-problem.ts
+++ src/01-unions/01-problem.ts
@@ -1,3 +1,9 @@
+type Colorful = { color: string };
+type Circle = { radius: number };
+
+// an intersection of types
+type ColorfulCircle = Colorful & Circle;
+
 // a union of types
 type SimpleUnion = "a" | "b" | "c";
 
@@ -5,16 +11,4 @@ type State = {
   status: "loading" | "success" | "error";
   data?: string[];
   error?: Error;
-};
-
-// @ts-expect-error
-const example: State = {
-  status: "success",
-  // where's data?
-};
-
-const example2: State = {
-  status: "loading",
-  // @ts-expect-error
-  error: new Error("whaaaa?"),
 };
